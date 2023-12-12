6f567dd17914c5c6ab8c0d29b7573d078bec91bb
Guillermo Gaona <guillermo_gaona@hotmail.com>
2023-12-12 15:03:31 -0300

    Agregado de Vetana añadir Tarea

--- app/src/main/java/com/proyecto/tasks/addtasks/ui/tasksScreen.kt
+++ app/src/main/java/com/proyecto/tasks/addtasks/ui/tasksScreen.kt
@@ -1,8 +1,78 @@
 package com.proyecto.tasks.addtasks.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+@Preview
+@Composable
+fun TasksScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        AddTasksDialog(true) {}
+        FabDialog(Modifier.align(Alignment.BottomEnd))
+    }
+}
 
 @Composable
-fun TasksScreen(tasksViewModel: TasksViewModel) {
+fun FabDialog(modifier: Modifier) {
+    FloatingActionButton(onClick = {
+        //Muestra Dialogo
+    }, modifier = modifier.padding(16.dp)) {
+        Icon(Icons.Filled.Add, contentDescription = "")
+    }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit) {
+    var myTask by remember { mutableStateOf("") }
+    if (show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(14.dp)) {
+                Text(
+                    text = "Añade una Tarea nueva",
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.SemiBold
+                )
+                Spacer(modifier = Modifier.size(14.dp))
+                TextField(value = myTask, onValueChange = { myTask = it }, singleLine = true)
+                Spacer(modifier = Modifier.size(14.dp))
+                Button(onClick = {
+                    //mandar tarea
+                }, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Añadir Tarea")
+                }
+            }
+        }
+    }
 }
